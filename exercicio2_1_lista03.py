fea2b17c89d64c3a0a645e2dfbb44cc991f91e97
Paulo Vinícius Moreira Dutra <121131923+dutrapaulovm@users.noreply.github.com>
2026-04-10 18:50:16 -0300

    Add files via upload

---
+++ exercicio2_1_lista03.py
@@ -0,0 +1,15 @@
+numero1 = input("Digite o valor 1: ")
+numero1 = int(numero1)
+
+numero2 = input("Digite o valor 2: ")
+numero2 = int(numero2)
+
+if numero1 > numero2:
+    print(f"O número {numero1} é maior que {numero2}")
+elif (numero2 > numero1):
+    print(f"O número {numero2} é maior que {numero1}")
+else:
+    print("Os número são iguais")
+
+
+
---
+++ exercicio2_2_lista03.py
@@ -0,0 +1,8 @@
+numero = input("Digite um valor: ")
+numero = float(numero)
+
+if numero >= 10 and numero <= 50:
+    print("Está entre os intervalos")
+else:
+    print("Fora dos intervalos")
+
